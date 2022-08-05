52f810ab8de1e82790b4eee59300cdaeb7ec8c34
Gabriel Lacroce <gp.lacroce@gmail.com>
2022-08-05 13:46:13 -0300

    Inicio implementação de validações

---
+++ src/DevIO.Business/Services/FornecedorService.cs
@@ -0,0 +1,33 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using DevIO.Business.Models.Validations;
+using System;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Services
+{
+    public class FornecedorService : BaseService, IFornecedorService
+    {
+        public async Task Adicionar(Fornecedor fornecedor)
+        {
+            if (!ExecutarValidacao(new FornecedorValidation(), fornecedor)) return;
+
+            return;
+        }
+
+        public Task Atualizar(Fornecedor fornecedor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AtualizarEndereco(Endereco endereco)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
